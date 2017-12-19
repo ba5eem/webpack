1605f11f5b600a72f4b2492a70f3aa9da6025e75
Ba5eem <baseem@gmail.com>
2017-12-19 10:02:21 -1000

    compile es6

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,15 @@ let config = {
   entry: './index.js',
   output: {
     filename: 'output.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/, //files ending with .js
+        exclude: /node_modules/, //exclude the node modules dir
+        loader: "babel-loader" // use this (babel-core) loader
+      }
+    ]
   }
 }
 
